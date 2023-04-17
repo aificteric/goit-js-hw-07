c0be5ebcafb78ff551c7611dc2e970435fda0c19
Anton Volvenko <volvenko.anthony@gmail.com>
2023-04-18 00:34:25 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,20 +2,20 @@
 import { galleryItems } from './gallery-items.js';
 
 //! Find the gallery container element in the HTML
-const container = document.querySelector('.gallery');
+const galleryContainer = document.querySelector('.gallery');
 
 //! Generate the HTML markup for the gallery items
-const markup = createGalleryMarkup(galleryItems);
+const galleryMarkup = createGalleryMarkup(galleryItems);
 
 //! Insert the HTML markup into the gallery container element
-container.insertAdjacentHTML('beforeend', markup);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
 //* Function that generates the HTML markup for each gallery item
 
 function createGalleryMarkup(items) {
   return items
     .map(({ preview, original, description }) => {
-      return `<div class="gallery__item">
+      return `<li class="gallery__item">
         <a class="gallery__link" href="${original.value}">
           <img
             class="gallery__image"
@@ -24,39 +24,31 @@ function createGalleryMarkup(items) {
             alt="${description}"
           />
         </a>
-      </div>`;
+      </li>`;
     })
     .join('');
 }
 
+//* Function to handle click events on the gallery container element
+
 const onContainerClick = (clickEvent) => {
   clickEvent.preventDefault();
-
-  // If the clicked element is not a gallery item, do nothing
   if (clickEvent.target.classList.contains('gallery')) return;
-
-  // Get the URL of the original image from the data-source attribute of the clicked image
   const source = clickEvent.target.dataset.source;
-
   // Create a new basicLightbox instance with the original image as its content
-  const instance = basicLightbox.create(`
-    <img src="${source}" width="800" height="600">
-  `);
-
-  // Show the lightbox instance
+  const instance = basicLightbox.create(
+    `<img src="${source}" width="800" height="600">`
+  );
   instance.show();
 
-  // Close the lightbox instance on Esc key press
+  //! Close the lightbox instance on Esc key press
   const onEscKeyDown = (event) => {
     if (event.code === 'Escape') {
       instance.close();
       window.removeEventListener('keydown', onEscKeyDown);
     }
   };
-
-  // Add a keydown event listener to the window object
   window.addEventListener('keydown', onEscKeyDown);
 };
 
-// Add a click event listener to the gallery container element
-container.addEventListener('click', onContainerClick);
+galleryContainer.addEventListener('click', onContainerClick);
